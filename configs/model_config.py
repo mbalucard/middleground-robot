fdf89ad23050342fa287b1a55b944d7efd3406bf
Da曙光wn <66196727+mbalucard@users.noreply.github.com>
2026-08-03 13:41:04 +0800

    [opt]优化模型推理力度参数

--- configs/model_config.py
+++ configs/model_config.py
@@ -24,12 +24,13 @@ def _read_env(name: str) -> str:
 
 class DeepSeekModelConfig:
     """DeepSeek模型配置"""
-    BASE_URL = _read_env("DEEPSEEK_BASE_URL")
+    BASE_URL_OPENAI = _read_env("DEEPSEEK_BASE_URL_OPENAI")
+    BASE_URL_ANTHROPIC = _read_env("DEEPSEEK_BASE_URL_ANTHROPIC")
     API_KEY = _read_env("DEEPSEEK_API_KEY")
     MODEL_NAME = "deepseek-v4-flash"
 
 class MiniMaxModelConfig:
-    BASE_URL = _read_env("MINIMAX_ANTHROPIC_URL")
+    BASE_URL_ANTHROPIC = _read_env("MINIMAX_ANTHROPIC_URL")
     API_KEY = _read_env("MINIMAX_KEY")
     MODEL_NAME = "anthropic:MiniMax-M2.7"
 
@@ -37,6 +38,7 @@ class Tavily:
     API_KEY = _read_env("TAVILY_API_KEY")
 
 if __name__ == "__main__":
-    print(DeepSeekModelConfig.BASE_URL)
-    print(MiniMaxModelConfig.BASE_URL)
+    print(DeepSeekModelConfig.BASE_URL_OPENAI)
+    print(DeepSeekModelConfig.BASE_URL_ANTHROPIC)
+    print(MiniMaxModelConfig.BASE_URL_ANTHROPIC)
     
--- robot/agents/models.py
+++ robot/agents/models.py
@@ -12,20 +12,21 @@ from configs.model_config import DeepSeekModelConfig,MiniMaxModelConfig
 deepseek_model = init_chat_model(
     model=DeepSeekModelConfig.MODEL_NAME,
     api_key=DeepSeekModelConfig.API_KEY,
-    base_url=DeepSeekModelConfig.BASE_URL,
+    base_url=DeepSeekModelConfig.BASE_URL_OPENAI,
     temperature=0,  # 温度，0为最确定，1为最不确定
     # disabled enabled
-    extra_body={"thinking":{"type": "disabled"}},  # 思考类型，disabled为不思考，enabled为思考
+    extra_body={"thinking":{"type": "enabled"}},  # 思考类型，disabled 为不思考， enabled 为思考
     model_provider="openai",  # 模型提供者，openai为openai，anthropic为anthropic
+    reasoning_effort="low",  # 推理力度，low为低，high为中，max为高
 )
 
 minimax_model = init_chat_model(
     model=MiniMaxModelConfig.MODEL_NAME,
     api_key=MiniMaxModelConfig.API_KEY,
-    base_url=MiniMaxModelConfig.BASE_URL,
+    base_url=MiniMaxModelConfig.BASE_URL_ANTHROPIC,
 )
 
 
 if __name__ == "__main__":
-    print(deepseek_model.model_name)
+    print(deepseek_model.model_dump)
     print(minimax_model.model_dump)
